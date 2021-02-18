c42f9694d1cc2cfa0d9bc688ef98fef498fd65b0
Tyler Farris <Tylerpfarris@gmail.com>
2021-02-18 14:21:34 -0800

    getitn there

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const { fruits } = require('./data.s');
 
+app.use(cors());
+
+app.get('/pathOne', (req, res) => {
+
+   
+    res.json({ path: 'one' });
+}
+);
+app.get('/fruit', (req, res) => {
+    
+    console.log('fruits', fruits);
+
+    res.json({ results: fruits });
+}
+);
+app.get('/pathOne', (req, res) => {
+
+    const id = Nuimber(req.params.id);
+
+    const selectedFruit = fruits.find((fruit) => fruit.id === id);
+
+    res.json({ results: selectedFruit });
+}
+);
+
+module.exports = {
+    app
+};
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const { app } = require('./app.js');
 
-const port = 5000
+const port = process.env.PORT || 5000;
 
-app.listen(port, ( => {
-    console.log(`running on http://localhost${port}`)
-}))
+app. listen(port, () => {
+    console.log(`running on http://localhost${port}`);
+}
+);
